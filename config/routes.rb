899b2d57372f3a7556066e01a004b6d922092a3d
Goruchie <marcosgualtero43253@gmail.com>
2023-05-24 19:23:51 -0300

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/users/marcosgualtero43253', to: 'users#show'
+  root 'users#show', id: 'marcosgualtero43253'
+
   resources :users
 end
