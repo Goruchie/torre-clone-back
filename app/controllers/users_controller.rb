e741442816821615765c4b716ae667cf162b3bf1
Goruchie <marcosgualtero43253@gmail.com>
2023-05-24 15:47:46 -0300

    Fix rubocop offenses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,12 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
+  require 'rest-client'
 
-    require 'rest-client'
-
-    def show
-        
-        username = params[:id]
-        url = "bio.torre.co/api/bios/" + username
-        response = RestClient.get(url)
-        render json: response
-
-    end
-
+  def show
+    username = params[:id]
+    url = "bio.torre.co/api/bios/#{username}"
+    response = RestClient.get(url)
+    render json: response
+  end
 end
